b19acda85a101d9e46d3cbb47bfcc908c74a277b
spjyotiranjan <jyotiranjan.sahoo@kalvium.community>
2023-12-07 16:32:47 +0530

    Done

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 import './App.css';
 import lake from "./image/lake.JPG";
 import GallaryFooter from './components/GallaryFooter';
+import { Header } from './components/Header';
+import { Main } from './components/Main';
 
 const imageData = [
   {
@@ -24,20 +26,9 @@ const imageData = [
 function App() {
   return (
     <div>
-      <header>
-        <img className="logo" src="https://s3.ap-south-1.amazonaws.com/kalvi-education.github.io/front-end-web-development/Kalvium-Logo.png" alt=""></img>
-        <nav>
-          <ul className='nav_links'>
-            <li><a href='#'>Home</a></li>
-            <li><a href='#'>About</a></li>
-            <li><a href='#'>More</a></li>
-          </ul>
+      <Header />
 
-        </nav>
-        <a className='cta' href='#'><button>Contact</button></a>
-      </header>
-
-      <div>
+      {/* <div>
         {
           imageData.map(image=>{
             return(
@@ -47,7 +38,8 @@ function App() {
             )
           })
         }
-      </div>
+      </div> */}
+      <Main imageData={imageData}/>
       {/* adding footer component */}
       <GallaryFooter/> 
     </div>
---
+++ src/components/Main.jsx
@@ -0,0 +1,15 @@
+export function Main(props){
+    return (
+        <div>
+        {
+          props.imageData.map(image=>{
+            return(
+              <div key={image.id} className="column">
+                <img src={image.img} alt=""></img>
+              </div>
+            )
+          })
+        }
+      </div>
+    )
+}
